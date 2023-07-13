e967e0f84cacaacf8d9648a0db9e43097eb28c0e
kimperria <kimwaweru100@gmail.com>
2023-07-13 15:26:51 +0300

    ft(todo): add update todo func - separate client and server logic - add toggleTodo function - persist updated data from endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,11 @@ function getTodos(){
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id: string, complete: boolean){
+  "use server"
 
+  await prisma.todo.update({ where: { id }, data: { complete }})
+}
 
 export default async function Home() {
 
@@ -27,7 +31,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map( todo => 
-          <TodoItem key={todo.id} {...todo}/>
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
           )}
       </ul>
     </>
